a545269d89f6b984a35783e6b3bb1d2bfe8c517f
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-03 21:13:31 +0500

    Update Site Number

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const SITE_CONFIG = {
   description: "Crafting Excellence Through Innovative Learning",
   contact: {
     email: "admissions@eduacademy.com",
-    phone: "+1 (555) 000-1234",
+    phone: "+92 0000000000",
     address: "123 Education Way, Tech City, TC 54321",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509455!2d-122.4194155!3d37.7749295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085809c6c8f4459%3A0xb10ed59c15a25362!2sSan%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1638361234567!5m2!1sen!2sus",
